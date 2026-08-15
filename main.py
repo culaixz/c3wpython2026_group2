80523c0bea55fd7daff1e2acb4476570f8a602af
victoriaaa12 <victoria.a.aung@gmail.com>
2026-08-15 09:22:25 -0500

    players working now

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ FPS = 30
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init() # sounds
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My game")
 clock = pygame.time.Clock()
-player = players.Player(200,100)
+
+p1 = players.Player(100, 300, player_num=1)
+p2 = players.Player(600, 300, player_num=2)
+arrow_list = []
+
 running = True
 while running:
     # Clock
@@ -25,10 +30,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     # Drawing
-    player.update()
+    p1.update(arrow_list)
+    p2.update(arrow_list)
 
-    screen.fill(color=BLUE)
-    player.draw(screen) # drawing the background
+    screen.fill(color=BLUE) # drawing the background
+    p1.draw(screen)
+    p2.draw(screen) 
     
     pygame.display.flip()
 # quit the game after the loop exits
