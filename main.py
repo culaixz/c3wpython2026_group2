85cdf1d1aeaac491c2bc40874e84ff4960c56bdb
victoriaaa12 <victoria.a.aung@gmail.com>
2026-08-15 09:37:22 -0500

    added player

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import pygame
-<<<<<<< HEAD
-import player
-=======
 import players
->>>>>>> players
 
 # Make constants
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 720
+HEIGHT = 720
 FPS = 30
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -20,13 +16,9 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My game")
 clock = pygame.time.Clock()
 
-<<<<<<< HEAD
-theplayer = player.Player(100,100)
-=======
 p1 = players.Player(100, 300, player_num=1)
 p2 = players.Player(600, 300, player_num=2)
 arrow_list = []
->>>>>>> players
 
 running = True
 while running:
@@ -42,12 +34,8 @@ while running:
     p2.update(arrow_list)
 
     screen.fill(color=BLUE) # drawing the background
-<<<<<<< HEAD
-    theplayer.draw(screen)
-=======
     p1.draw(screen)
     p2.draw(screen) 
->>>>>>> players
     
     pygame.display.flip()
 # quit the game after the loop exits
