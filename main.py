0eb2e8b04d0dc145254ae6ea0095f340ccd0ce0c
victoriaaa12 <victoria.a.aung@gmail.com>
2026-08-16 13:39:24 -0500

    added archery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import players 
+import archery
 
 # Make constants
 WIDTH = 720
@@ -19,6 +20,8 @@ clock = pygame.time.Clock()
 p1 = players.Player(100, 300, player_num=1)
 p2 = players.Player(600, 300, player_num=2)
 arrow_list = []
+maze_walls = []
+is_game_over = archery.update_arrows(arrow_list, maze_walls, p1, p2)
 
 running = True
 while running:
@@ -29,13 +32,33 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if is_game_over and archery.game_over_event(event):
+            running = False
+
+    if not is_game_over:
+        p1.update(arrow_list)
+        p2.update(arrow_list)
+
     # Drawing
+    screen.fill(color=BLUE) # drawing the background
+
+    p1.draw(screen)
+    p2.draw(screen) 
+
     p1.update(arrow_list)
     p2.update(arrow_list)
 
-    screen.fill(color=BLUE) # drawing the background
-    p1.draw(screen)
-    p2.draw(screen) 
+    for arrow in arrow_list:
+        arrow.draw(screen)
+
+    archery.update_arrows(arrow_list, maze_walls, p1, p2)
+
+    archery.draw_hearts(screen, p1, 20, 35, "Player 1")
+    archery.draw_hearts(screen, p2, 450, 35, "Player 2")
+
+    if is_game_over:
+        archery.draw_game_over(screen, p1, p2)
     
     pygame.display.flip()
 # quit the game after the loop exits
