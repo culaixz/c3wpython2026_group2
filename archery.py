9204f8c89082f6a6136746d5724a2f94c6812634
思安留白 <alchiimez@gmail.com>
2026-08-20 01:33:24 -0400

    Update archery.py
    fixed indentation

--- archery.py
+++ archery.py
@@ -125,7 +125,7 @@ def draw_game_over(screen, player1, player2):
     )
     screen.blit(result_text, result_rect)
 
-   hint = info_font.render(
+    hint = info_font.render(
     "Press ESC to quit",
     True,
     (220, 220, 220)
